a3ef3e700d53eb31c4238726260c52aaba652bae
Linkedinky <81112372+Linkedeline@users.noreply.github.com>
2023-10-22 20:19:14 -0400

    Update main.py
    added Database

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from time import sleep
 import os
 import json
+import sqlite3
 import requests
-Discord_token = "you're token here"
-Discord_ID = "You're discord user ID here"
+Discord_token = "You're token"
+Discord_ID = "you're ID"
 HEADERS = {"authorization": Discord_token, "content-type": "application/json"}
 def download_image(url, save_path):
     response = requests.get(url)
@@ -13,12 +14,10 @@ def download_image(url, save_path):
         print(f"Image downloaded successfully and saved as {save_path}")
     else:
         print("Failed to download image.")
-
 def make_my_profile(user,relation):
     name = user['user']['username']
     theID = user['user']['id']
     avatar = user['user']['avatar']
-
     img = f"https://cdn.discordapp.com/avatars/{theID}/{avatar}.png?size=4096"
     download_image(img,name+'.png')
     print(name)
@@ -39,16 +38,17 @@ def make_my_profile(user,relation):
     "# Common friends\n" + \
     f"{c}\n" + \
     f"![[{name}.png]]"
+    
     return [filename, line]
 def make_profile(user,relation):
     name = user['user']['username']
     avatar = user['user']['avatar']
     theID = user['user']['id']
     img = f"https://cdn.discordapp.com/avatars/{theID}/{avatar}.png?size=4096"
-    print(img)
     download_image(img,name+'.png')
     print(name)
-    print(relation)
+    add_DB(theID,name)
+   
     d = ''
     for i in user['mutual_guilds']: d = d + f"{i['id']} "
     c = ''
@@ -80,25 +80,52 @@ def create_and_write_file(file_name,line):
         print("An error occurred while creating or writing to the file.")
         # Usage example
 
+def add_DB(userID, username):
+    con = sqlite3.connect('user.db')
+    c = con.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users  (id  TEXT PRIMARY KEY, username TEXT)''')
+    c.execute("INSERT OR REPLACE INTO users (id, username) VALUES (?,?)", (userID, username))
+    con.commit()
+    con.close()
+   
+    
+def check_user(user_id):
+    con = sqlite3.connect('user.db')
+    c = con.cursor()
+    c.execute("SELECT EXISTS(SELECT 1 FROM users WHERE id = ?)", (user_id,))
+    result = c.fetchone()[0]
+    exists = True if result == 1 else False
+    con.close()
+    return exists
+
 url0 = "https://discord.com/api/users/@me/relationships"
 friends = requests.get(url0, headers=HEADERS)
 urlme = f"https://discord.com/api/users/{Discord_ID}/profile"
 profile = requests.get(urlme, headers=HEADERS)
+print (profile)
 meinfo = make_my_profile(profile.json(), friends.json())
-#create_and_write_file(meinfo[0],meinfo[1])
-
-for i in friends.json(): 
-    ID =int(i['user']['id'])
+create_and_write_file(meinfo[0],meinfo[1])
+total = len([ 'i' for friends in friends.json()])
+passing, failling, skipped = 0, 0 ,0 
+for i in friends.json():
+    ID =str(i['user']['id'])
     url1 = f"https://discord.com/api/users/{ID}/profile" 
     url2 = f"https://discord.com/api/users/{ID}/relationships" 
     friend = requests.get(url1, headers=HEADERS)
     relation = requests.get(url2, headers=HEADERS)
     try:
-        #print(friend)
+    #print(friend)
         
-        info = make_profile(friend.json(), relation.json())
-        create_and_write_file(info[0],info[1])
+        if not (check_user(ID)):
+            info = make_profile(friend.json(), relation.json())
+            create_and_write_file(info[0],info[1])
+            passing += 1
+            sleep(1)
+        else:
+            skipped += 1
+            print("user exist and skipped")
     except Exception as e: 
         print(e)
-    print("loading... ")
-    sleep(4)
+        failling += 1
+    os.system('clear')
+    print(f"loading... \nPassing: {passing}\nfailed: {failling}\nskipped: {skipped}\n{passing+skipped+failling}/{total} ")
